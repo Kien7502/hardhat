060c86b47a24f227392f349cf93462e96f00387c
KIEN\Nguyen Sam <trungkien07052002@gmail.com>
2023-08-23 10:39:01 +0700

    23-8

--- nft-erc1155/hardhat.config.js
+++ nft-erc1155/hardhat.config.js
@@ -1,21 +1,23 @@
 /**
  * @type import('Hardhat/config').HardhatUserConfig
  */
+require("dotenv").config();
 require("@nomiclabs/hardhat-ethers");
+const EVN_KEY = process.env.SECRET_KEY
 module.exports = {
   solidity: "0.8.1",
   networks: {
     goerli: {
       url: "https://ethereum-goerli.publicnode.com",
-      accounts: ["86af082eb388d2d5dccc744dce6eed3bf3c4f63c01e54da948ccf03b5195fd3c"]
+      accounts: [EVN_KEY]
     },
     bnb: {
       url: "https://bsc-testnet.publicnode.com",
-      accounts: ["86af082eb388d2d5dccc744dce6eed3bf3c4f63c01e54da948ccf03b5195fd3c"]
+      accounts: [EVN_KEY]
     },
     fantom: {
       url: "https://fantom-testnet.publicnode.com",
-      accounts: ["86af082eb388d2d5dccc744dce6eed3bf3c4f63c01e54da948ccf03b5195fd3c"]
+      accounts: [EVN_KEY]
     }
   },
 
